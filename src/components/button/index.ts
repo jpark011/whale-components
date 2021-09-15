547ea8ade570bdebac1ab208c468902b8630ac5b
Jay Park <wngt3319@gmail.com>
2021-09-16 01:02:41 +0900

    refactor styles out

--- src/components/button/index.ts
+++ src/components/button/index.ts
@@ -1,66 +1,13 @@
-import {css, html, LitElement, TemplateResult} from 'lit';
+import {html, LitElement, TemplateResult} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {classMap} from 'lit/directives/class-map.js';
-import {grey, primary, primaryDark, primaryLight, white, whiteDark} from '../../utils/colors';
+import {primaryStyle, secondaryStyle} from './style';
 
 export type WlButtonType = 'primary' | 'secondary';
 
-const style = css`
-  :host {
-    --wl-button-text-color: var(--wl-color-grey-100, ${white});
-
-    --wl-button-background-color: var(--wl-color-primary-500, ${primary});
-    --wl-button-hover-background-color: var(--wl-color-primary-300, ${primaryLight});
-    --wl-button-active-background-color: var(--wl-color-primary-700, ${primaryDark});
-    --wl-button-disabled-background-color: var(--wl-color-grey-500, ${grey});
-
-    display: inline-block;
-  }
-
-  :host([type='secondary']) {
-    --wl-button-text-color: var(--wl-color-primary-500, ${primary});
-
-    --wl-button-background-color: var(--wl-color-grey-100, ${white});
-    --wl-button-hover-background-color: var(--wl-color-grey-300, ${whiteDark});
-    --wl-button-active-background-color: var(--wl-color-grey-500, ${primaryLight});
-  }
-
-  button {
-    background-color: var(--wl-button-background-color);
-    color: var(--wl-button-text-color);
-    border: none;
-    border-radius: 5px;
-    padding: 8px 16px;
-    font-size: 1rem;
-    font-weight: bold;
-    cursor: pointer;
-    display: flex;
-    align-items: center;
-    gap: 4px;
-  }
-
-  button.secondary {
-    border: 2px solid var(--wl-button-text-color);
-  }
-
-  button:hover {
-    background-color: var(--wl-button-hover-background-color);
-    color: var(--wl-button-text-color);
-  }
-
-  button:active {
-    background-color: var(--wl-button-active-background-color);
-  }
-
-  button:disabled {
-    background-color: var(--wl-button-disabled-background-color);
-    cursor: not-allowed;
-  }
-`;
-
 @customElement('wl-button')
 export default class WlButton extends LitElement {
-  static styles = [style];
+  static styles = [primaryStyle, secondaryStyle];
 
   @property({reflect: true})
   type: WlButtonType = 'primary';
---
+++ src/components/button/style.ts
@@ -0,0 +1,57 @@
+import {css} from 'lit';
+import {grey, primary, primaryDark, primaryLight, white, whiteDark} from '../../utils/colors';
+
+export const primaryStyle = css`
+  :host {
+    --wl-button-text-color: var(--wl-color-grey-100, ${white});
+
+    --wl-button-background-color: var(--wl-color-primary-500, ${primary});
+    --wl-button-hover-background-color: var(--wl-color-primary-300, ${primaryLight});
+    --wl-button-active-background-color: var(--wl-color-primary-700, ${primaryDark});
+    --wl-button-disabled-background-color: var(--wl-color-grey-500, ${grey});
+
+    display: inline-block;
+  }
+
+  button {
+    background-color: var(--wl-button-background-color);
+    color: var(--wl-button-text-color);
+    border: none;
+    border-radius: 5px;
+    padding: 8px 16px;
+    font-size: 1rem;
+    font-weight: bold;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  }
+
+  button:hover {
+    background-color: var(--wl-button-hover-background-color);
+    color: var(--wl-button-text-color);
+  }
+
+  button:active {
+    background-color: var(--wl-button-active-background-color);
+  }
+
+  button:disabled {
+    background-color: var(--wl-button-disabled-background-color);
+    cursor: not-allowed;
+  }
+`;
+
+export const secondaryStyle = css`
+  :host([type='secondary']) {
+    --wl-button-text-color: var(--wl-color-primary-500, ${primary});
+
+    --wl-button-background-color: var(--wl-color-grey-100, ${white});
+    --wl-button-hover-background-color: var(--wl-color-grey-300, ${whiteDark});
+    --wl-button-active-background-color: var(--wl-color-grey-500, ${primaryLight});
+  }
+
+  button.secondary {
+    border: 2px solid var(--wl-button-text-color);
+  }
+`;
